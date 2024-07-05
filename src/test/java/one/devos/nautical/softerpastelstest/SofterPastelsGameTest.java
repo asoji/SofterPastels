56af95d31bf38ed9b8d50eedb4de7bd6cf36ef1c
Asoji <asoji-git@hiitsdevin.mozmail.com>
2024-07-05 00:03:19 -0700

    ✅ Added the only gametest ever needed for this mod
    this is either the funniest shit ever or the dumbest shit ever

--- src/test/java/one/devos/nautical/softerpastelstest/SofterPastelsGameTest.java
+++ src/test/java/one/devos/nautical/softerpastelstest/SofterPastelsGameTest.java
@@ -1,19 +1,23 @@
 package one.devos.nautical.softerpastelstest;
 
 import net.fabricmc.fabric.api.gametest.v1.FabricGameTest;
+import net.minecraft.core.BlockPos;
 import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.gametest.framework.GameTestHelper;
-import net.minecraft.world.level.block.Blocks;
 import one.devos.nautical.softerpastels.common.SofterPastelsBlocks;
 
 public class SofterPastelsGameTest implements FabricGameTest {
-    @GameTest(template = SofterPastelsTest.MOD_ID + ":powderturnedblock", required = true, requiredSuccesses = 3)
+    // Softer Pastel only need *1* game test, which is fucking funny considering how many hours Jay has been trying to
+    // help me and Carter figure out game tests while Carter actively struggled to get a Game Test env setup, which
+    // isn't his fault but oof ow ouch owie his legs
+    @GameTest(template = SofterPastelsTest.MOD_ID + ":powderturnedblock", required = true, requiredSuccesses = 3, setupTicks = 5)
     public void powderTurnedBlock(GameTestHelper helper) {
-        helper.setBlock(0, 2, 1, Blocks.REDSTONE_BLOCK);
-        helper.setBlock(0, 2, 2, Blocks.REDSTONE_BLOCK);
-        helper.setBlock(0, 2, 3, Blocks.REDSTONE_BLOCK);
-        helper.succeedWhenBlockPresent(SofterPastelsBlocks.INSTANCE.getLIGHT_RED_PASTEL_BLOCK(), 3, 1, 1);
-        helper.succeedWhenBlockPresent(SofterPastelsBlocks.INSTANCE.getLIGHT_GREEN_PASTEL_BLOCK(), 3, 1, 1);
-        helper.succeedWhenBlockPresent(SofterPastelsBlocks.INSTANCE.getLIGHT_BLUE_PASTEL_BLOCK(), 3, 1, 1);
+        helper.pressButton(0, 4, 2);
+
+        helper.succeedWhen(() -> {
+            helper.assertBlock(new BlockPos(3, 2, 3), (block) -> block == SofterPastelsBlocks.INSTANCE.getLIGHT_RED_PASTEL_BLOCK(), "Expected Light Red Pastel Block");
+            helper.assertBlock(new BlockPos(3, 2, 2), (block) -> block == SofterPastelsBlocks.INSTANCE.getLIGHT_GREEN_PASTEL_BLOCK(), "Expected Light Green Pastel Block");
+            helper.assertBlock(new BlockPos(3, 2, 1), (block) -> block == SofterPastelsBlocks.INSTANCE.getLIGHT_BLUE_PASTEL_BLOCK(), "Expected Light Blue Pastel Block");
+        });
     }
 }
